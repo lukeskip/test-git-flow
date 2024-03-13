a2295b8a86a00bc832b869286b7ee4097e963612
Japheth Ramirez <japheth.ra369@gmail.com>
2024-03-13 10:47:36 -0600

    Guardado

--- Angel/angel.js
+++ Angel/angel.js
@@ -1,5 +1,2 @@
-console.log('Hola Mundo')
-console.log('Hola Mundo 2')
-console.log('Hola Mundo 3')
-console.log('Hola Mundo 4')
-console.log('Hola Mundo 5')
+console.log('Angel')
+console.log('Ramirez')
